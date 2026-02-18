2e7fbbee58935ab963b49b663b88c4177e9b8284
Portfolio Developer <dev@portfolio.local>
2026-02-18 15:51:13 -0300

    finish commision

--- src/commission.js
+++ src/commission.js
@@ -3,4 +3,13 @@ function calculateCommission(amount, rate) {
   return amount * rate;
 }
 
+function calculateProgressiveCommission(amount, rate) {
+  // TODO: implement progressive rates
+  if (amount < 100) {
+    return amount * rate;
+  } else if (amount < 1000) {
+    return amount;
+  }
+}
+
 module.exports = { calculateCommission };
